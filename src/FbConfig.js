0a03e9ec5f19736e33136b9c5a946ddfec10b287
mohamed osman <devothman512@gmail.com>
2019-04-20 01:50:33 +0200

    Update FbConfig.js

--- src/FbConfig.js
+++ src/FbConfig.js
@@ -1,4 +1,5 @@
 export default {
-    appId : ' Your Key in facebook api ',
-    mapAPIKey : 'APi Google '
+    appId : '344010706454649',
+    mapAPIKey : 'AIzaSyAqU5D3w9gXH5Mk7w8cRzwMW1Gt8jiANfI'
+    //mapAPIKey : 'AIzaSyCkSOc-v8UoRcreXrcDrp3Hwxeta1IYKFk'
 };
