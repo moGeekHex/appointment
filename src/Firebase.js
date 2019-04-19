51c3a93c9cb905c06266be88c3f5f9cec85e74d9
mohamed osman <devothman512@gmail.com>
2019-04-20 01:51:09 +0200

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  	`your config in firebase`
+  apiKey: "AIzaSyDdg3RSXndrPq6iYnZX26RELmZEq-xeaoE",
+  authDomain: "appointment-832ca.firebaseapp.com",
+  databaseURL: "https://appointment-832ca.firebaseio.com",
+  projectId: "appointment-832ca",
+  storageBucket: "appointment-832ca.appspot.com",
+  messagingSenderId: "963721858239"
 };
 firebase.initializeApp(config);
 
